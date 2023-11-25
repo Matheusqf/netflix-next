f06803c21d5e7bf8b5641f1506c6395d62fddb51
matheus queiroz <matheus.ica@gmail.com>
2023-11-25 02:31:26 +0000

    Getting started, setup auth UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-2x1 text-green-500">Netflix Next</h1>
+      <h1 className="text-2x1 text-green-500">Netflix Next 1</h1>
+      <div className="relative h-full w-full bg-[url('/images/hero.jpg')]">
+        a<div>teste</div>
+      </div>
     </>
-  )
+  );
 }
