a2147f65d1ca0c7d6f6d7448bceb1c67a2a11a00
matheus queiroz <matheus.ica@gmail.com>
2023-11-29 23:04:45 +0000

    Add profiles screen

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,27 +1,21 @@
 "use client";
 
 import axios from "axios";
-import { useCallback, useState } from "react";
 import Image from "next/image";
 import Input from "@/components/auth/Input";
 import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
-
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import { useCallback, useState } from "react";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
-  const router = useRouter();
 
   const [variant, setVariant] = useState("login");
-
   const toggleVariant = useCallback(() => {
-    setVariant((currentVariant) =>
-      currentVariant === "login" ? "register" : "login"
-    );
+    setVariant((prev) => (prev === "login" ? "register" : "login"));
   }, []);
 
   const login = useCallback(async () => {
@@ -29,14 +23,12 @@ const Auth = () => {
       await signIn("credentials", {
         email,
         password,
-        redirect: false,
-        callbackUrl: "/",
+        callbackUrl: "/profiles",
       });
-      router.push("/");
     } catch (error) {
       console.log(error);
     }
-  }, [email, password, router]);
+  }, [email, password]);
 
   const register = useCallback(async () => {
     try {
@@ -89,22 +81,24 @@ const Auth = () => {
               onClick={variant === "login" ? login : register}
               className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
             >
-              {variant === "login" ? "Login" : "Sign up"}
+              {variant === "login" ? "Sign In" : "Sign up"}
             </button>
-
-            <div className="flex flex-row gap-4 items-center mt-8 justify-center">
-              <div
-                onClick={() =>
-                  signIn("google", { callbackUrl: "/", redirect: true })
-                }
-                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
-              >
+            <div
+              onClick={() => {
+                signIn("google", { callbackUrl: "/profiles", redirect: true });
+              }}
+              className="flex flex-row gap-4 items-center mt-8 justify-center"
+            >
+              <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                 <FcGoogle size={30} />
               </div>
               <div
-                onClick={() =>
-                  signIn("github", { callbackUrl: "/", redirect: true })
-                }
+                onClick={() => {
+                  signIn("github", {
+                    callbackUrl: "/profiles",
+                    redirect: true,
+                  });
+                }}
                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
               >
                 <FaGithub size={30} />
